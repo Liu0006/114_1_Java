f6adde9ccd95b70c919ef19827d09e28792cb55e
Liu0006 <a113223006@mail.shu.edu.tw>
2025-11-14 15:00:29 +0800

    114_1_Java

---
+++ 114-11-15/Inheritance01/src/ShieldSwordMAn.java
@@ -0,0 +1,20 @@
+public class ShieldSwordMAn extends  SwordsMan{
+    private int defenseCapacity;
+    //建構子:初始化
+    public ShieldSwordMAn(String name, int health, int attackPower, int defenseCapacity){
+        super(name, health, attackPower);
+        this.defenseCapacity = defenseCapacity;
+    }
+
+
+    @Override
+    public void attack(Role opponent){
+        int reducedDamage = this.getAttackPower() -5;
+        opponent.setHealth((opponent.getHealth() - reducedDamage));
+        System.out.println(this.getName() + "揮劍攻擊" + opponent.getName() + "造成" + reducedDamage +"點傷害" + opponent);
+    }
+
+    public int getDefenseCapacity(){
+        return defenseCapacity;
+    }
+}
